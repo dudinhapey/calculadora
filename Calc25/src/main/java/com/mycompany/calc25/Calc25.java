b61ef2daed9cf11f0f0cea288dde449bf8d0b81d
Lucas <lucasschimidtt16@gmail.com>
2025-04-14 20:41:21 -0300

    Criando a classe UI

---
+++ Calc25/src/main/java/com/mycompany/calc25/Calc25.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.calc25;
+
+/**
+ *
+ * @author 08219
+ */
+public class Calc25 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
