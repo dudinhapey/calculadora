fab48d77bb892bdc4e1c64c114b47f4d82c7d598
Eduarda <eduarda.soligo@duque.g12.br>
2025-04-14 20:28:46 -0300

    Adicionando as operações

---
+++ Calc2025/src/main/java/com/mycompany/calc2025/OperationHandler.java
@@ -0,0 +1,71 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.Calc2025;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ *
+ * @author 08211
+ */
+public class OperationHandler {
+
+    static double num1 = 0, num2 = 0, result = 0;
+    static char operator;
+
+    public static void connect(CalculatorUI ui) {
+
+        ui.addButton.addActionListener(e -> {
+            num1 = Double.parseDouble(ui.textfield.getText());
+            operator = '+';
+            ui.textfield.setText("");
+        });
+
+        ui.subButton.addActionListener(e -> {
+            num1 = Double.parseDouble(ui.textfield.getText());
+            operator = '-';
+            ui.textfield.setText("");
+        });
+
+        ui.mulButton.addActionListener(e -> {
+            num1 = Double.parseDouble(ui.textfield.getText());
+            operator = '*';
+            ui.textfield.setText("");
+        });
+
+        ui.divButton.addActionListener(e -> {
+            num1 = Double.parseDouble(ui.textfield.getText());
+            operator = '/';
+            ui.textfield.setText("");
+        });
+
+        ui.equButton.addActionListener(e -> {
+            num2 = Double.parseDouble(ui.textfield.getText());
+            switch (operator) {
+                case '+': result = num1 + num2; break;
+                case '-': result = num1 - num2; break;
+                case '*': result = num1 * num2; break;
+                case '/': result = num1 / num2; break;
+            }
+            ui.textfield.setText(String.valueOf(result));
+            num1 = result;
+        });
+
+        ui.clearButton.addActionListener(e -> ui.textfield.setText(""));
+
+        ui.delButton.addActionListener(e -> {
+            String text = ui.textfield.getText();
+            if (!text.isEmpty()) {
+                ui.textfield.setText(text.substring(0, text.length() - 1));
+            }
+        });
+
+        ui.negButton.addActionListener(e -> {
+            double temp = Double.parseDouble(ui.textfield.getText());
+            temp *= -1;
+            ui.textfield.setText(String.valueOf(temp));
+        });
+    }
+}
