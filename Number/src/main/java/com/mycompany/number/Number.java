dde887860e746de8b44c3a0aefd1ef591959541f
Mainara <mainara.silva@duque.g12.br>
2025-04-14 20:47:26 -0300

    criação dos botões dos números

---
+++ Number/src/main/java/com/mycompany/number/Number.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.number;
+
+/**
+ *
+ * @author 08208
+ */
+public class Number {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
