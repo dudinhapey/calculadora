bb267d0d7777cba1197233e96ee91e37f88b4e4d
Eduarda <eduarda.soligo@duque.g12.br>
2025-04-14 20:25:26 -0300

    Primeiro commit

---
+++ Calc2025/src/main/java/com/mycompany/calc2025/Calc2025.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.calc2025;
+
+/**
+ *
+ * @author 08211
+ */
+public class Calc2025 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
